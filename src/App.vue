1982767c84e1b640259d32f074364db462b28604
DongHyeok Yang <lukigreen@naver.com>
2021-06-30 16:48:15 +0900

    init portfolio

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id ="app">
+    <Menu></Menu>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Menu from './components/header/menu.vue'
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    HelloWorld, Menu,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/header/menu.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<nav class="navbar navbar-expand-sm bg-navbar-light">
+			<ul class="nav  justify-content-center">
+				<li class="nav-item"><router-link class="nav-link" to="/boardlist">WHO</router-link></li>
+				<li class="nav-item"><router-link class="nav-link" to="/boardlist">ABOUT</router-link></li>
+				<li class="nav-item"><router-link class="nav-link" to="/boardlist">RESUME</router-link></li>
+				<li class="nav-item"><router-link class="nav-link" to="/boardlist">PORTFOLIO</router-link></li>
+				<li class="nav-item"><router-link class="nav-link" to="/boardlist">SERVICE</router-link></li>
+				<li class="nav-item"><router-link class="nav-link" to="/boardlist">CONTACT</router-link></li>
+			</ul>
+		</nav>
+	</div>
+</template>
+
+<style>
+.navbar .navbar-nav {
+  display: inline-block;
+  float: none;
+}
+
+.navbar .navbar-collapse {
+  text-align: center;
+}
+</style>
